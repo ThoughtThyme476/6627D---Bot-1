5462d82910364eddfddcc4c705b870e6da6edbe5
ThoughtThyme476 <cole.herro@icloud.com>
2025-06-25 20:06:30 -0700

    worked on mcl more

--- 6627D-Bot1/src/mcl.cpp
+++ 6627D-Bot1/src/mcl.cpp
@@ -10,7 +10,7 @@
 const int  N_PARTICLES = 300;
 const double FIELD_WIDTH = 3650.0;
 const double FIELD_LENGTH = 3650.0;
-const double SENDOR_NOISE_STD = 50.0;
+const double SENSOR_NOISE_STD = 50.0;
 const double ODOMETRY_NOISE_STD = 5.0;
 const double ROTATION_NOISE_STD = 1.0;
 
@@ -81,9 +81,50 @@ void updateParticleWithMotion() {
 
     std::normal_distribution<double> transNoise(0.0, ODOMETRY_NOISE_STD);
     std::normal_distribution<double> rotNoise(0.0, ODOMETRY_NOISE_STD);
+
+    for (Particle& p : particles){
+        double noisy_d = delta_d + transNoise(rng);
+        double noisy_theta = delta_heading + rotNoise(rng);
+
+        p.theta =+ noisy_theta;
+        if(p.theta < 0) p.theta += 360;
+        if(p.theta >= 0) p.theta -= 360;
+
+        double theta_rad = p.theta* M_PI /180.00;
+
+        p.x += noisy_d * cos(theta_rad);
+        p.y += noisy_d * sin(theta_rad);
+
+        if(p.x < 0) p.x = 0;
+        if(p.x > FIELD_WIDTH) p.x = FIELD_WIDTH;
+        if(p.x < 0) p.y = 0;
+        if(p.x > FIELD_LENGTH) p.y = FIELD_LENGTH;
+    }
 }
 
+void updateParticlesWithSensor() {
+    int32_t z_left = dist_left.get();
+    int32_t z_right = dist_right.get();
+    if(z_left < 0) z_left = FIELD_WIDTH;
+    if(z_right < 0) z_right = FIELD_LENGTH;
 
+    double weight_sum = 0.0; 
+    double var = SENSOR_NOISE_STD * SENSOR_NOISE_STD;
+    for(Particle& p : particles) {
+
+        double pred_left = p.x;
+        double pred_right = FIELD_WIDTH - p.x; 
+
+        double err_left = z_left - pred_left;
+        double err_right = z_right - pred_right;
+
+        double w_left = exp(-(err_left * err_left) / (2*var));
+        double w_right = exp(-(err_right * err_right) / (2*var));
+        p.weight = w_left * w_right;
+        weight_sum += p.weight;
+    }
+
+}
 
 
 
